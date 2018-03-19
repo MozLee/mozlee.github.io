7f807789a193dd2353cf3bc6e484692fbc9cc3c7
MozLee <lixin@mozlee.com>
2018-03-19 13:46:57 +0800

    Site updated: 2018-03-19 13:46:56

--- js/src/bootstrap.js
+++ js/src/bootstrap.js
@@ -50,6 +50,8 @@ $(document).ready(function () {
 
   $(document).trigger('bootstrap:after');
 });
-console.log("Welcome to MozLee's Blog\n%c菩提本无树，明镜亦非台\n我的微博ID是 瞅啥那",'background:#aaa;color:#bada55');
+console.log("%c你要给我两个选择，一个是越老越值钱的行业，熬资历磨日子混关系，\n压着你的都是哆哆嗦嗦的大叔人老珠黄的阿姨；\n等老到不能享受的时候才实现了财富自由；\n一个是年纪轻轻就有很多机会，\n日新月异，千变万化，不学习就要失业，不奋斗就要贫穷，\nwork hard play harder，工作起劲，玩得尽兴。\n打死我都要选第二个。","text-shadow: 0 0 2px #0789e0;color: #fff;background:#0eb0f6");
+console.log('%c程序员是一碗青春饭，吃了它永葆青春!!!~','font-size:20px;text-shadow: 0 0 2px #0789e0;color:#fff;background:#0eb0f6;');
+console.log("%c", "padding:90px 700px;background:url('http://mozlee.com/uploads/mmz.gif')no-repeat;");
 
 
